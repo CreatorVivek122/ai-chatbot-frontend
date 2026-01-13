b8fe2927bddcfb7a72f4158c1821a7572b2ade76
creatorvivek122 <redekarvivek126@gmail.com>
2026-01-13 18:28:13 +0530

    Updated markdown

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
+import { provideMarkdown } from 'ngx-markdown';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideHttpClient() // ✅ THIS FIXES IT
+    provideHttpClient(),
+    provideMarkdown()
   ]
 });
